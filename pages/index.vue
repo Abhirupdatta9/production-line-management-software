a2d8e80072961a8d076e2d146150dcaf1ebeff3f
Abhirupdatta9 <dattaabhirup712@gmail.com>
2020-04-10 01:06:41 +0530

    operator workdesk

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,23 @@
 <template>
 <div>
-  <v-img src="/bg2.jpg">
+  <v-img src="/bg2.jpg" height="100vh" width="100vw">
     <v-container fluid>
       <v-row>
-        <v-col >
-            <div class="float-right " style="background-color:rgba(255,255,255,0.5);">
+        <v-col cols="7"></v-col>
+        <v-col cols="4">
+            <div style="background-color:rgba(255,255,255,0.5);">
               <v-card width="400px" height="450px" style="margin-top:80px; margin-left:90px; padding-bottom:30px" class="justify-right">
-                <v-app-bar>
-                  <v-card-text primary-title text-color="#5e35b1" class="justify-center display-1 font-weight-black " style="text-align:center; size=20px">USER LOGIN</v-card-text>
-                </v-app-bar>
+                <!-- <v-app-bar color="#5e35b1"> -->
+                  <v-card-text class="justify-center  display-1 font-weight-black " style="text-align:center; size=20px">USER LOGIN</v-card-text>
+                <!-- </v-app-bar> -->
                 <v-row>
                   <v-col offset="1" cols="10">
                     <v-form style="margin-top:50px;" @submit.prevent="submit">
-                      <v-text-field solo prepend-icon="mdi-account" class="my-input" :type="'email'" label="Enter UserID" required style="margin-bottom:-20px" v-model="form.email"></v-text-field>
-                      <v-text-field solo prepend-icon="mdi-lock" label="Enter Password" class="my-input" :type="'password'" required style="margin-bottom:-20px" v-model="form.password"></v-text-field>
-                      <v-select :items="items" prepend-icon="mdi-ticket-account" solo label="User Type" required  class="my-input" v-model="form.name"></v-select>
+                      <v-text-field solo rounded prepend-icon="mdi-account" :type="'email'" label="Enter User ID" required style="margin-bottom:-20px" v-model="form.email"></v-text-field>
+                      <v-text-field solo rounded prepend-icon="mdi-lock" label="Enter Password" :type="'password'" required style="margin-bottom:-20px" v-model="form.password"></v-text-field>
+                      <v-select :items="items" prepend-icon="mdi-ticket-account" solo rounded label="User Type" required v-model="form.name"></v-select>
                       <v-row>
-                  <v-col cols="10" offset="1" float>
+                  <v-col cols="11" offset="1" float>
                     <v-btn type="submit" block large rounded dark color="#5e35b1" style="margin-bottom=-5px;">Login</v-btn>
                   </v-col>
                   <v-col offset="1" style="color:grey;margin-bottom=50">
@@ -52,7 +53,7 @@ export default {
         data: this.form
         })
       //console.log(response)
-      this.$router.push("/incharge/incharge-login");
+      this.$router.push("/operator/dashboard");
       }catch(e)
       {
         console.log(e);
--- pages/operator/workdesk.vue
+++ pages/operator/workdesk.vue
@@ -1,57 +1,83 @@
 <template>
-<div >
-  <v-card outlined height:100>
-      <v-container grid-list-md text-xs-center>
-        <v-layout row wrap>
-            <v-flex xs2>
-                <v-text-field label="Line Number" v-model="lineNumber" required></v-text-field>
-            </v-flex>
-            <v-flex xs2>
-                <v-text-field v-model="stationNumber" label="Station Number" required></v-text-field>
-            </v-flex>
-            <v-flex xs2 style="padding-top:'10px'">
-                <v-btn @click="showlineStatus" color="#5e35b1" dark>Enter</v-btn>
-            </v-flex>
-     </v-layout>
-    </v-container>
-  </v-card>
+  <v-row>
+    <v-col cols="8" offset="2">
 
-  <v-card style="background-color: #5e35b1" height="390px" dark>
-    <v-row>
-      <v-col cols="4" offset="4">
-        <v-card-text style="font-family:'roboto'; text-size:'2em'" v-if="entered" ><p class="display-1 text--primary">Welcome ! You are in station {{stationNumber}} of line {{lineNumber}}</p></v-card-text> 
-      </v-col>
-    </v-row>
-    
-    <br><br><br>
-    <v-row>
-      <v-col cols="2" offset="3">
-        <v-btn block class="teal" dark >Scan Sub-Assembly</v-btn>
-      </v-col>
-    </v-row>
-    <br>
+      <v-row>
+        <v-col >
+          <v-text-field
+            label="Line Number"
+            outlined
+            dense
+            readonly
+          ></v-text-field>
+        </v-col>
+        <v-col >
+          <v-text-field
+            label="Station Number"
+            outlined
+            dense
+            readonly
+          ></v-text-field>
+        </v-col>
+        <v-col >
+          <v-btn outlined >Report error</v-btn>
+        </v-col>
+        <v-col >
+          <v-btn outlined >Start scanning</v-btn>
+        </v-col>
+        <v-col >
+          <v-btn outlined>Stop scanning</v-btn>
+        </v-col>
+      </v-row>
 
-    <v-row>
-      <v-col cols="6" offset="3">
-        <div class="text-center">
-          <div>
-            <v-btn class="ma-2" color="primary" dark>Scan Incoming Part
-            </v-btn>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <v-btn class="ma-2" color="primary" dark>Accept
-              <v-icon dark right>mdi-checkbox-marked-circle</v-icon>
-            </v-btn>
-            &nbsp;&nbsp;&nbsp;&nbsp;
-            <v-btn class="ma-2" color="red" dark>Decline
-              <v-icon dark right>mdi-cancel</v-icon>
-            </v-btn>
-          </div>
-        </div>
-      </v-col>
-    </v-row>
 
-  </v-card>
-</div>
+      <v-card outlined height="600px" class="pa-5">
+        <center><p class="display-1 font-weight-light">Scan Interface</p></center>
+        <v-container>
+          <v-row>
+
+            <v-col>
+              <v-card outlined class=" px-5 py-3" height="400px">
+                <center><h2 class="font-weight-light">Incoming Part</h2></center>
+                <br>
+                <v-row dense v-for="item in part" :key="item" >
+                  <v-col>
+                    <v-text-field 
+                      :label="item.label"
+                      :value="item.value"
+                      dense
+                      outlined
+                      readonly
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-card>
+            </v-col>
+
+            <v-col>
+              <v-card outlined class="px-5 py-3" height="400px">
+                <center><h2 class="font-weight-light">Subassembly</h2></center>
+                <br>
+                <v-row dense v-for="item in subAssembly" :key="item" >
+                  <v-col>
+                    <v-text-field
+                      :label="item.label"
+                      :value="item.value"
+                      dense
+                      outlined
+                      readonly
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-card>
+            </v-col>
+
+          </v-row>
+        </v-container>
+      </v-card>
+      
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -61,13 +87,34 @@ export default {
         return{
           lineNumber:'',
           stationNumber:'',
-          entered:0
+
+          part: [
+            {
+              label: "Incoming part name",
+              value: "Steering wheel"
+            },
+            {
+              label: "Incoming part id",
+              value: "ST_nano_101"
+            },
+          ],
+
+          subAssembly: [
+            {
+              label: "Incoming subassembly model",
+              value: "Nano"
+            },
+            {
+              label: "Incoming subassembly ID",
+              value: "Nan_C1024_mcq"
+            },
+          ]
+
+
         }
     },
     methods : {
-      showlineStatus(){
-        this.entered=1
-      }
+    
     }
 }
 </script>
