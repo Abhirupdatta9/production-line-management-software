ace72f59e66e2d0832f6b3d34e82c1e8e0122091
Di-sha <dishaa.k1412@gmail.com>
2020-04-09 13:03:23 +0530

    updated index.vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,66 +1,79 @@
 import pkg from './package'
 
 export default {
-  mode: 'spa',
+    mode: 'spa',
 
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '@/plugins/particles',
-  ],
-
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    '@nuxtjs/vuetify',
-    // 'nuxt-material-design-icons'
-  ],
-  /*
-  ** Axios module configuration
-  */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
-
-  /*
-  ** Build configuration
-  */
-  build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
+     ** Headers of the page
+     */
+    head: {
+        title: pkg.name,
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: pkg.description }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: '#fff' },
+
+    /*
+     ** Global CSS
+     */
+    css: [],
+
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [
+        '@/plugins/particles',
+    ],
+
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        '@nuxtjs/pwa',
+        '@nuxtjs/vuetify',
+        '@nuxtjs/auth'
+        // 'nuxt-material-design-icons'
+    ],
+
+
+    /*
+     ** Axios module configuration
+     */
+    axios: {
+        baseURL: "http://localhost:8000/api"
+    },
+
+    auth: {
+        strategies: {
+            local: {
+                endpoints: {
+                    login: { url: '/login', method: 'post', propertyName: 'meta.token' },
+                    logout: { url: '/logout', method: 'post' },
+                    user: { url: '/user', method: 'get', propertyName: 'data' }
+                },
+            }
+        }
+    },
+
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
     }
-  }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -4,27 +4,26 @@
     <v-container fluid>
       <v-row>
         <v-col >
-            <div class="float-right " style="background-color:rgba(255,255,255,0.5);width=50%;">
+            <div class="float-right " style="background-color:rgba(255,255,255,0.5);">
               <v-card width="400px" height="450px" style="margin-top:80px; margin-left:90px; padding-bottom:30px" class="justify-right">
                 <v-app-bar>
                   <v-card-text primary-title text-color="#5e35b1" class="justify-center display-1 font-weight-black " style="text-align:center; size=20px">USER LOGIN</v-card-text>
                 </v-app-bar>
                 <v-row>
                   <v-col offset="1" cols="10">
-                    <v-form style="margin-top:50px;">
-                      <v-text-field solo prepend-icon="mdi-account" class="my-input" :type="'number'" label="Enter UserID" required style="margin-bottom:-20px"></v-text-field>
-                      <v-text-field solo prepend-icon="mdi-lock" label="Enter Password" class="my-input" :type="'password'" required style="margin-bottom:-20px"></v-text-field>
-                      <v-select :items="items" prepend-icon="mdi-ticket-account" solo label="User Type"  class="my-input"></v-select>
-                    </v-form>
-                  </v-col>
-                </v-row>
-          
-                <v-row>
+                    <v-form style="margin-top:50px;" @submit.prevent="submit">
+                      <v-text-field solo prepend-icon="mdi-account" class="my-input" :type="'email'" label="Enter UserID" required style="margin-bottom:-20px" v-model="form.email"></v-text-field>
+                      <v-text-field solo prepend-icon="mdi-lock" label="Enter Password" class="my-input" :type="'password'" required style="margin-bottom:-20px" v-model="form.password"></v-text-field>
+                      <v-select :items="items" prepend-icon="mdi-ticket-account" solo label="User Type" required  class="my-input" v-model="form.name"></v-select>
+                      <v-row>
                   <v-col cols="10" offset="1" float>
-                    <v-btn block large rounded dark color="#5e35b1" to="dashboard" style="margin-bottom=-5px;">Login</v-btn>
+                    <v-btn type="submit" block large rounded dark color="#5e35b1" style="margin-bottom=-5px;">Login</v-btn>
                   </v-col>
                   <v-col offset="1" style="color:grey;margin-bottom=50">
                     <nuxt-link to="/" style="text-decoration:none;" text-color="grey">Forgot password?</nuxt-link>
+                  </v-col>
+                </v-row>
+                    </v-form>
                   </v-col>
                 </v-row>
               </v-card>
@@ -41,7 +40,25 @@ export default {
 
     data: () => ({
       items: ['Operator', 'Incharge', 'Manager'],
+      form:{
+        email:'',
+        password:'',
+    }
     }),
+    methods:{
+    async submit(){
+      try{
+    await this.$auth.loginWith('local',{
+        data: this.form
+        })
+      //console.log(response)
+      this.$router.push("/incharge/incharge-login");
+      }catch(e)
+      {
+        console.log(e);
+      }
+  }
+  }
 
 }
 </script>
