b0f89f3b4ee8118b0b156ef3538cc3849534f1f8
banerjee-suchismita <suchismita.b07@gmail.com>
2020-05-24 22:43:50 +0530

    Update quality-manager

--- pages/quality assurance/manager.vue
+++ pages/quality assurance/manager.vue
@@ -5,83 +5,49 @@
             <v-col cols="8" offset="2">
                 <v-card outlined class="px-3 py-3">
                     <v-row>
-                        <v-col cols="6">
+                        <v-col>
                             <v-data-table
                             :headers="headers"
-                            :items="stations"
-                            :items-per-page="5"
-                            class="elevation-1"
+                            :items="quality"
+                                :items-per-page="5"
+                                class="elevation-1"
                             >
-                            <template v-slot:top>
-                                <v-toolbar flat>
-                                <v-toolbar-title>Quality Assessment Status Table</v-toolbar-title>
-                                
-                                </v-toolbar>
-                            </template>
-                            <template  v-slot:item.operator="{ item }">
-                                <v-select
-                                v-if="item.assigned != true"
-                                :items="items"
-                                label="Select status"
-                                outlined
-                                dense
-                                class="mt-2 mb-n5"
-                                ></v-select>
-                                <template v-else>
-                                {{item.operator}}
+
+                                <template v-slot:top>
+                                    <v-toolbar flat>
+                                    <v-toolbar-title>Quality Assessment Status Table</v-toolbar-title>
+                                    </v-toolbar>
                                 </template>
-                            </template>
-                            <template  v-slot:item.name="{ }">
-                                <v-text-field
-                                label="Comments"
-                                outlined
-                                dense
-                                class="mt-2 mb-n5"
-                                >
-                                </v-text-field>
-                            </template>
-                            </v-data-table>
-                        </v-col>
-                        
-                        <v-col cols="6">
-                            <v-data-table
-                            :headers="headers"
-                            :items="stations"
-                            :items-per-page="5"
-                            class="elevation-1"
-                            >
-                            <template v-slot:top>
-                                <v-toolbar flat>
-                                <v-toolbar-title>On Hold Table</v-toolbar-title>
-                                
-                                </v-toolbar>
-                            </template>
-                            <template  v-slot:item.operator="{ item }">
-                                <v-select
-                                v-if="item.assigned != true"
-                                :items="items"
-                                label="Select status"
-                                outlined
-                                dense
-                                class="mt-2 mb-n5"
-                                ></v-select>
-                                <template v-else>
-                                {{item.operator}}
+
+                                <template  v-slot:item.operator="{}">
+                                    <v-select       
+                                    :items="items"
+                                    label="Select status"
+                                    outlined
+                                    dense
+                                    class="mt-2 mb-n5"
+                                    ></v-select>
                                 </template>
-                            </template>
-                            <template  v-slot:item.name="{ }">
-                                <v-text-field
-                                label="Comments"
-                                outlined
-                                dense
-                                class="mt-2 mb-n5"
-                                >
-                                </v-text-field>
-                            </template>
+                                
+                                <template  v-slot:item.name="{}">
+                                    <v-text-field
+                                    label="Comments"
+                                    outlined
+                                    dense
+                                    class="mt-2 mb-n5"
+                                    >
+                                    </v-text-field>
+                                </template>
+
                             </v-data-table>
                         </v-col>
                     </v-row>
                 </v-card>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="2" offset="5">
+                 <v-btn block outlined @click="update()">Update Data</v-btn>
             </v-col>
         </v-row>
     </div>
@@ -91,48 +57,40 @@ export default {
     layout: 'quality',
     data() {
         return{
-       
-            items: [
-                "Clear", "Hold"
-            ],
+            parts : [],
+            items: [ "Clear", "Hold"],
             headers: [
-            {
-                text: 'Subassembly ID',
-                align: 'start',
-                sortable: false,
-                value: 'number',
-                
-            },
-            { text: 'Status', value: 'operator', width: 180 },
-            { text: 'Comments', value: 'name',width: 180 },
+                { text: 'Subassembly ID', align: 'start', sortable: false, value: "number" },
+                { text: 'Status', value: "operator", width: 180 },
+                { text: 'Comments',value: "name", width: 700 },
             ],
-            stations: [
+            quality: [
                 {
-                number: 'S01',
+                qnumber: '',
                 name: 'Wheels',
                 operator: 'honey singh' ,
                 assigned: false,
                 },
                 {
-                number: 'S02',
+                qnumber: '',
                 name: 'Doors',
                 operator: 'honey singh' , 
                 assigned: false, 
                 },
                 {
-                number: 'S03',
+                qnumber: '',
                 name: 'Engine',
                 operator: 'honey singh' , 
                 assigned: false, 
                 },
                 {
-                number: 'S04',
+                qnumber: '',
                 name: 'Dashboard',
                 operator: 'honey singh' , 
                 assigned: false, 
                 },
                 {
-                number: 'S05',
+                qnumber: '',
                 name: 'Seats',
                 operator: 'honey singh' , 
                 assigned: false, 
@@ -140,6 +98,28 @@ export default {
             ],
 
         }
+    },
+
+    methods : {
+        update() {
+
+        }
+    },
+
+    mounted: function () {
+        this.$nextTick(async function () {
+                let response = await this.$axios.$get('quality');
+                this.parts = response.data
+                console.log(this.parts)
+
+                this.quality.forEach((i)=>{i.qnumber=this.parts.forEach( function (id) {return id.uniqueParts})})
+
+                console.log(this.quality)
+
+                // console.log(this.parts.forEach((id)=>{return id.uniqueParts}))
+
+                // this.parts.forEach(function (e) {console.log(e.uniqueParts);})
+        })
     }
 }
 </script>
